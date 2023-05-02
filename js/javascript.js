da2ed5070d7986b80476ab53de21120071a29fed
risenyard <61482588+risenyard@users.noreply.github.com>
2023-05-02 20:46:19 +0300

    -

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,20 @@
+// add OSM basemap
 let map = L.map('map').setView([58.373523, 26.716045], 12)
-const osm =
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
- maxZoom: 19,
- attribution: 'OpenStreetMap contributors',
-})
+
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
 osm.addTo(map)
+
+// add geoJSON polygons layer
+async function addDistrictsGeoJson(url) {
+    const response = await fetch(url)
+    const data = await response.json()
+    const polygons = L.geoJson(data)
+    polygons.addTo(map)
+   }
+
+addDistrictsGeoJson("geojson/tartu_city_districts_edu.geojson")
+   
